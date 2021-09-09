9457d5e93800576d7b39be52915583bc699aabee
mahy209 <60543525+mahy209@users.noreply.github.com>
2021-09-10 01:57:13 +0200

    day 7

---
+++ day 7/task 2/stylescript.js
@@ -0,0 +1,121 @@
+function fontArial (){
+    document.getElementById("demo").style.fontFamily = "Arial";
+}
+function fontGeorgia (){
+    document.getElementById("demo").style.fontFamily = "georgia";
+}
+function fontArialBlack (){
+    document.getElementById("demo").style.fontFamily = "Arial Black";
+}
+function fontVerdana (){
+    document.getElementById("demo").style.fontFamily = "Verdana";
+}
+function fontImpact (){
+    document.getElementById("demo").style.fontFamily = "impact";
+}
+
+function font1 (){
+    document.getElementById("demo").style.fontSize = "12px";
+}
+function font2 (){
+    document.getElementById("demo").style.fontSize = "14px";
+}
+function font3 (){
+    document.getElementById("demo").style.fontSize = "16px";
+}
+function font4 (){
+    document.getElementById("demo").style.fontSize = "18px";
+}
+function font5 (){
+    document.getElementById("demo").style.fontSize = "20px";
+}
+
+function align1(){
+    document.getElementById("demo").style.textAlign = "left";
+}
+function align2(){
+    document.getElementById("demo").style.textAlign = "right";
+}
+function align3(){
+    document.getElementById("demo").style.textAlign = "center";
+}
+function align4(){
+    document.getElementById("demo").style.textAlign = "justify";
+}
+
+function height1(){
+    document.getElementById("demo").style.lineHeight = "normal";
+}
+function height2(){
+    document.getElementById("demo").style.lineHeight = "10px";
+}
+function height3(){
+    document.getElementById("demo").style.lineHeight = "15px";
+}
+function height4(){
+    document.getElementById("demo").style.lineHeight = "1.5em";
+}
+
+function tranform1(){
+    document.getElementById("demo").style.textTransform = "none";
+}
+function tranform2(){
+    document.getElementById("demo").style.textTransform = "capitalize";
+}
+function tranform3(){
+    document.getElementById("demo").style.textTransform = "uppercase";
+}
+function tranform4(){
+    document.getElementById("demo").style.textTransform = "lowercase";
+}
+
+function decor1(){
+    document.getElementById("demo").style.textDecoration = "none";
+}
+function decor2(){
+    document.getElementById("demo").style.textDecoration = "line-through";
+}
+function decor3(){
+    document.getElementById("demo").style.textDecoration = "overline";
+}
+function decor4(){
+    document.getElementById("demo").style.textDecoration = "underline";
+}
+
+function border1(){
+    document.getElementById("demo").style.border = "none";
+}
+function border2(){
+    document.getElementById("demo").style.border = "dotted";
+}
+function border3(){
+    document.getElementById("demo").style.border = "double";
+}
+function border4(){
+    document.getElementById("demo").style.border = "groove";
+}
+function border5(){
+    document.getElementById("demo").style.border = "dashed";
+}
+function border6(){
+    document.getElementById("demo").style.border = "solid";
+}
+
+function bordercolor1(){
+    document.getElementById("demo").style.borderColor = "none";
+}
+function bordercolor2(){
+    document.getElementById("demo").style.borderColor = "pink";
+}
+function bordercolor3(){
+    document.getElementById("demo").style.borderColor = "red";
+}
+function bordercolor4(){
+    document.getElementById("demo").style.borderColor = "green";
+}
+function bordercolor5(){
+    document.getElementById("demo").style.borderColor = "blue";
+}
+function bordercolor6(){
+    document.getElementById("demo").style.borderColor = "purple";
+}
