b559fa6b8527161975df77a9ec795e4c716bc61f
mahy209 <60543525+mahy209@users.noreply.github.com>
2021-09-15 14:00:19 +0200

    week 3 day7,8,9,10

---
+++ day 9/task 3/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+	$('.add-items').submit(function(event) 
+	{
+		event.preventDefault();
+		var item = $('#todo-list-item').val();
+		if(item) 
+		{
+			$('#list-items').append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a><hr></li>");
+			$('#todo-list-item').val("");
+		}
+	});
+	$(document).on('change', '.checkbox', function() 
+	{
+		if($(this).attr('checked')) 
+		{
+			$(this).removeAttr('checked');
+		}else 
+		{
+			$(this).attr('checked', 'checked');
+		}
+		$(this).parent().toggleClass('completed');
+	});
+	$(document).on('click', '.remove', function() 
+	{
+		$(this).parent().remove();
+	});
+
+});
