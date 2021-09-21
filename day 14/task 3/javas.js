4be6dde6b58bc33a03bbc085e08349c2437541d2
mahy209 <60543525+mahy209@users.noreply.github.com>
2021-09-22 01:52:20 +0200

    day 14

---
+++ day 14/task 3/javas.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.vid-item').each(function(index){
+        $(this).on('click', function(){
+            var current_index = index+1;
+            $('.vid-item .thumb').removeClass('active');
+            $('.vid-item:nth-child('+current_index+') .thumb').addClass('active');
+        });
+    });
+});
